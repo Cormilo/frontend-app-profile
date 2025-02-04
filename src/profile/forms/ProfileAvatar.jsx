6c8141a36f8d213e69a80a50c12e748bbfb622b7
Cormilo <gitgitgithub@yandex.ru>
2025-02-04 23:54:47 +0000

    ?

--- src/profile/forms/ProfileAvatar.jsx
+++ src/profile/forms/ProfileAvatar.jsx
@@ -113,15 +113,13 @@ class ProfileAvatar extends React.Component {
   renderAvatar() {
     const { intl } = this.props;
 
-    return this.props.isDefault ? (
-      <DefaultAvatar className="text-muted" role="img" aria-hidden focusable="false" viewBox="0 0 24 24" />
-    ) : (
+    return (
       <img
         data-hj-suppress
         className="w-100 h-100 d-block rounded-circle overflow-hidden"
         style={{ objectFit: 'cover' }}
         alt={intl.formatMessage(messages['profile.image.alt.attribute'])}
-        src={`url(${avatar})`}
+        src={avatar}
       />
     );
   }
